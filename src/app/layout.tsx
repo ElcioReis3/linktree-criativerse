9c66e55c8fd4448dd7967b680ee67c7cf52b4160
Élcio Reis <elcio.reis024@gmail.com>
2025-07-29 12:23:29 -0300

    new tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,23 @@ import { Poppins } from "next/font/google";
 export const metadata: Metadata = {
   title: "Linktree Criativerse",
   description: "Conheça nossas redes sociais",
+
+  openGraph: {
+    title: "Página de links",
+    description: "Clique em nossos links e conheça-nos.",
+    url: "https://criativerse-design-cx.netlify.app/",
+    siteName: "Criativerse Design",
+    images: [
+      {
+        url: "https://criativerse-design-cx.netlify.app/_next/image?url=%2Fimages%2Flogo.png&w=640&q=75", // imagem de destaque
+        width: 1200,
+        height: 630,
+        alt: "Imagem de capa para compartilhamento",
+      },
+    ],
+    locale: "pt_BR",
+    type: "website",
+  },
 };
 
 const poppins = Poppins({
